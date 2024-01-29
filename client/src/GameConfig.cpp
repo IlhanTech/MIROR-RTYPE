b50199981f70940d707d687dc9049ebb50c565d4
IlhanTech <71987215+IlhanTech@users.noreply.github.com>
2024-01-29 02:10:53 +0100

    update

--- client/src/GameConfig.cpp
+++
@@ -1,7 +0,0 @@
-#include "../include/GameConfig.hpp"
-
-namespace Config {
-    std::string GameTitle = "R-Type";
-    sf::Vector2f ButtonSize = sf::Vector2f(125, 125);
-    sf::Vector2f SettingsGameButtonSize = sf::Vector2f(100, 100);
-}
